4ec854014f54fdc99ff5fd36380f93c5289cfc1c
Jawad Muhammadi <muhammadijawad10@gmail.com>
2018-11-30 18:54:13 -0800

    added newrelic

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -6,53 +7,20 @@ const compression = require('compression');
 
 app.use(compression());
 
-// app.get('/', (req, res) => {
-//   res.redirect('movies/1');
-// })
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get("/movies/:id", function(req, res) {
-//   const reactPath = path.join(__dirname, "public/index.html");
-//   res.sendFile(reactPath);
-// });
-
-app.use('/api/movies/:movieId/summary', 
-  proxy({
-    target: 'http://localhost:3007'
-  })
-)
 
 app.use('/api/movies/:movieid/rating', 
   proxy({
-    target: 'http://localhost:3013'
+    target: 'http://18.191.59.6/'
   })
 )
 
 app.get('/api/movies/:movieid/reviews', 
   proxy({
-    target: 'http://localhost:3013'
+    target: 'http://18.191.59.6/'
   })
 )
-
-app.get('/api/movies/:genre/relatedmovies',
-  proxy({
-    target: 'http://localhost:3003'
-  })
-)
-
-app.get('/api/movies/:movie/:date/:location',
-  proxy({
-    target: 'http://localhost:3002'
-  })
-)
-
-app.get('/api/moviesbyid/:movieid/:date/:location',
-  proxy({
-    target: 'http://localhost:3002'
-  })
-)
-
 app.listen(3000, () => {
   console.log('listening at port 3000');
 })
+
